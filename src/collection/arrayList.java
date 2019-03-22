791a9d4eaf5415eb81768c59d24f74044925d293
IgorMakhon <igor.mahon.blr@gmail.com>
2019-03-22 18:48:53 +0300

    friday practice in office with array

---
+++ src/collection/arrayList.java
@@ -0,0 +1,25 @@
+package collection;
+
+import java.util.ArrayList;
+
+public class arrayList {
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 4, 5, 6};
+        int b = 2;
+        int[] divisibleBy (a, b);
+    }
+}
+    public class EvenNumbers {
+        public static int[] divisibleBy(int[] numbers, int divider) {
+            int[] c;
+            for (int i=0; i<numbers.length;i++){
+                int z = numbers[i]%divider;
+                if(z!=0){
+                int c = c.add(numbers[i]);
+                }
+            }
+            return c;
+        }
+    }
+
+
